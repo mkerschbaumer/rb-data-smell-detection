2b294715031c743d2a868961dfb429178ac333f0
Martin Kerschbaumer <m.kerschbaumer@student.uibk.ac.at>
2021-04-01 14:04:09 +0200

    Add unit tests for GreatExpectationsDataset and GreatExpectationsDatasetManager

---
+++ data_smell_detection/tests/detectors/great_expectations/test_dataset.py
@@ -0,0 +1,51 @@
+import os
+import great_expectations
+
+from datasmelldetection.detectors.great_expectations.dataset import GreatExpectationsDatasetManager
+from datasmelldetection.core import Dataset
+
+cwd = os.getcwd()
+
+# NOTE: From view of root directory of package
+_test_data_directory = os.path.join(cwd, "tests/test_sets")
+_test_great_expectations_directory = os.path.join(cwd, "../great_expectations")
+
+manager = GreatExpectationsDatasetManager(
+    context_root_dir=_test_great_expectations_directory,
+    data_directory=_test_data_directory
+)
+
+
+class TestGreatExpectationsDatasetManager:
+    def test_creation(self):
+        GreatExpectationsDatasetManager(
+            context_root_dir=_test_great_expectations_directory,
+            data_directory=_test_data_directory
+        )
+
+    def test_get_available_dataset_identifiers(self):
+        identifiers = manager.get_available_dataset_identifiers()
+        assert isinstance(identifiers, set)
+        assert len(identifiers) == 1
+        assert "Titanic.csv" in identifiers
+
+    def test_get_dataset(self):
+        dataset = manager.get_dataset("Titanic.csv")
+        assert isinstance(dataset, Dataset)
+
+
+class TestGreatExpectationsDataset:
+    def test_get_column_names(self):
+        dataset = manager.get_dataset("Titanic.csv")
+
+        expected_column_names = ["Name", "PClass", "Age", "Sex", "Survived", "SexCode"]
+        column_names = dataset.get_column_names()
+        # Ensure all expected column names are present
+        assert all([name in column_names for name in expected_column_names])
+
+    def test_get_great_expectations_dataset(self):
+        dataset = manager.get_dataset("Titanic.csv")
+
+        # Extract Great Expectations dataset
+        ge_dataset = dataset.get_great_expectations_dataset()
+        assert isinstance(ge_dataset, great_expectations.dataset.Dataset)
