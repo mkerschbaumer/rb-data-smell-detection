c3ee7cb9024cd66016ad0eb414b504c44936ce25
Martin Kerschbaumer <m.kerschbaumer@student.uibk.ac.at>
2021-04-03 13:33:43 +0200

    Implement ExpectColumnValuesToNotContainMissingValueSmell

---
+++ data_smell_detection/datasmelldetection/detectors/great_expectations/expectations/expect_column_values_to_not_contain_missing_value_smell.py
@@ -0,0 +1,36 @@
+import json
+from great_expectations.expectations.core import ExpectColumnValuesToNotBeNull
+from great_expectations.profile.base import ProfilerDataType
+
+from datasmelldetection.core.datasmells import DataSmellType
+from datasmelldetection.detectors.great_expectations.datasmell import (
+    DataSmell,
+    DataSmellMetadata
+)
+
+
+class ExpectColumnValuesToNotContainMissingValueSmell(ExpectColumnValuesToNotBeNull, DataSmell):
+    """
+    Detect if a missing value smell is present.
+
+    The ExpectColumnValuesToNotBeNull expectation from Great Expectations
+    is used.
+    """
+
+    data_smell_metadata = DataSmellMetadata(
+        data_smell_type=DataSmellType.MISSING_VALUE_SMELL,
+        # Data smell for all supported data types
+        profiler_data_types=set([e for e in ProfilerDataType])
+    )
+
+    # NOTE: library_metadata not set since the ExpectColumnValuesToNotBeNull
+    # expectation sets it.
+
+
+# Perform registration of data smell at DataSmellRegistry
+expectation = ExpectColumnValuesToNotContainMissingValueSmell()
+expectation.register_data_smell()
+
+if __name__ == "__main__":
+    diagnostics_report = expectation.run_diagnostics()
+    print(json.dumps(diagnostics_report, indent=2))
