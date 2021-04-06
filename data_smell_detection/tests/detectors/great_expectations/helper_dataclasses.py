324ad6cbfc712f8aa2548c1c8b3903abe90ac44d
Martin Kerschbaumer <m.kerschbaumer@student.uibk.ac.at>
2021-04-06 10:47:35 +0200

    Add DataSmellInformation testing helper dataclass

---
+++ data_smell_detection/tests/detectors/great_expectations/helper_dataclasses.py
@@ -0,0 +1,20 @@
+from dataclasses import dataclass
+from typing import Any, Dict, Optional
+
+from datasmelldetection.detectors.great_expectations.datasmell import DataSmellMetadata
+
+# General information about a data smell. This dataclass is primarily intended
+# to store information about data smells in order to test their presence in
+# data smell registries or expectation suites.
+@dataclass
+class DataSmellInformation:
+    # Metadata about a data smell (e.g. column types and smell type which is
+    # detected).
+    metadata: DataSmellMetadata
+
+    # The expectation type of an Expectation class (of Great Expectations)
+    # which implements the data smell detection.
+    expectation_type: str
+
+    # Kwargs which should be used to evaluate the expectation.
+    kwargs: Optional[Dict[str, Any]]
