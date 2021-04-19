a528265df6cda1f5d772b5b1e8f5264b148a825b
Laura Geiger <laura.geiger@student.uibk.ac.at>
2021-04-19 11:22:50 +0200

    Integraded detector

--- web_application/argon-dashboard-django/app/views.py
+++ web_application/argon-dashboard-django/app/views.py
@@ -19,9 +19,14 @@ from app.models import File, Column, DetectedSmell
 cwd = os.getcwd()
 sys.path.append("/home/loryg/Documents/Gitlab/bachelor-thesis/data_smell_detection/")
 from datasmelldetection.detectors.great_expectations.dataset import GreatExpectationsDatasetManager
+from datasmelldetection.detectors.great_expectations.context import GreatExpectationsContextBuilder
+from datasmelldetection.detectors.great_expectations.detector import DetectorBuilder
+from datasmelldetection.detectors.great_expectations.detector import GreatExpectationsDetector
 
 from datasmelldetection.core.detector import DetectionStatistics, DetectionResult
 from datasmelldetection.core.datasmells import DataSmellType
+
+
 
 def index(request):
     
@@ -68,7 +73,13 @@ def upload(request):
 
 
 def smells(request):
+    believability_smells = [DataSmellType.DUMMY_VALUE_SMELL, DataSmellType.DUPLICATED_VALUE_SMELL, DataSmellType.EXTREME_VALUE_SMELL, DataSmellType.MEANINGLESS_VALUE_SMELL, DataSmellType.MISSPELLING_SMELL, DataSmellType.SUSPECT_CLASS_VALUE_SMELL, DataSmellType.SUSPECT_DATE_VALUE_SMELL, DataSmellType.SUSPECT_DATE_TIME_INTERVAL_SMELL, DataSmellType.SUSPECT_SIGN_SMELL, DataSmellType.SUSPECT_DISTRIBUTION_SMELL]
+    syntactic_understandability_smells = [DataSmellType.AMBIGUOUS_DATE_TIME_FORMAT_SMELL, DataSmellType.AMBIGUOUS_VALUE_SMELL, DataSmellType.CASING_SMELL, DataSmellType.CONTRACTING_SMELL, DataSmellType.EXTRANEOUS_VALUE_SMELL, DataSmellType.INTERMINGLED_DATA_TYPE_SMELL, DataSmellType.LONG_DATA_VALUE_SMELL, DataSmellType.MISSING_VALUE_SMELL, DataSmellType.SEPARATING_SMELL, DataSmellType.SPACING_SMELL, DataSmellType.SPECIAL_CHARACTER_SMELL, DataSmellType.SYNONYM_SMELL, DataSmellType.TAGGING_SMELL]
+    encoding_understandability_smells = [DataSmellType.DATE_AS_DATE_TIME_SMELL, DataSmellType.DATE_AS_STRING_SMELL, DataSmellType.DATE_TIME_AS_STRING_SMELL, DataSmellType.FLOATING_POINT_NUMBER_AS_STRING_SMELL, DataSmellType.INTEGER_AS_FLOATING_POINT_NUMBER_SMELL, DataSmellType.INTEGER_AS_STRING_SMELL, DataSmellType.TIME_AS_STRING_SMELL, DataSmellType.SUSPECT_CHARACTER_ENCODING_SMELL]
+    consistency_smells = [DataSmellType.ABBREVIATION_INCONSISTENCY_SMELL, DataSmellType.CASING_INCONSISTENCY_SMELL, DataSmellType.CLASS_INCONSISTENCY_SMELL, DataSmellType.DATE_TIME_FORMAT_INCONSISTENCY_SMELL, DataSmellType.MISSING_VALUE_INCONSISTENCY_SMELL, DataSmellType.SEPARATING_INCONSISTENCY_SMELL, DataSmellType.SPACING_INCONSISTENCY_SMELL, DataSmellType.SPECIAL_CHARACTER_INCONSISTENCY_SMELL, DataSmellType.SYNTAX_INCONSISTENCY_SMELL, DataSmellType.UNIT_INCONSISTENCY_SMELL, DataSmellType.TRANSPOSITION_INCONSISTENCY_SMELL]
+    all_smells = {'Believability Smells': believability_smells, 'Syntactic Understandability Smells': syntactic_understandability_smells, 'Encoding Understandability Smells': encoding_understandability_smells, 'Consistency Smells': consistency_smells}
     context = {}
+    context['smells'] = all_smells
     if request.method == 'POST':
       some_var = request.POST.getlist('smells')  
       context['list'] = some_var
@@ -76,23 +87,27 @@ def smells(request):
 
 def result(request):
     context = {}
-    outer = os.path.join(os.getcwd(), '../')
-    manager = GreatExpectationsDatasetManager(
-        context_root_dir = os.path.join(outer, '../great_expectations'),
-        data_directory = os.path.join(cwd, 'app/test_sets')
+    outer = os.path.join(os.getcwd(), "../")
+
+
+    context_builder = GreatExpectationsContextBuilder(
+        os.path.join(outer, "../great_expectations"),
+        os.path.join(cwd, "app/test_sets")
     )
-    dataset = manager.get_dataset('Titanic.csv')
+    con = context_builder.build()
+
+    manager = GreatExpectationsDatasetManager(context=con)
+    available_files = manager.get_available_dataset_identifiers()
+    dataset = manager.get_dataset("Titanic.csv")
     column_names = precheck_columns(dataset.get_column_names())
-    context['column_names'] = sorted(column_names)
-    uploaded_file = os.path.join(cwd, 'app/test_sets')
-    uploaded_file = File(path_to_file = os.path.join(uploaded_file, 'Titanic.csv'))
-    
-    statistic = DetectionStatistics(total_element_count = 50,faulty_element_count = 10)
-    smell_results = [DetectionResult(data_smell_type = 'DummyValueSmell', column_name = 'Name', statistics = statistic, faulty_elements = list()), DetectionResult(data_smell_type = 'DummyValueSmell', column_name = 'Survived', statistics = statistic, faulty_elements = list()), DetectionResult(data_smell_type = 'DummyValueSmell', column_name = 'Age', statistics = statistic, faulty_elements = list()), DetectionResult(data_smell_type = 'MissingValueSmell', column_name = 'Name', statistics = statistic, faulty_elements = list())]
-    sorted_results = sort_results(smell_results, column_names)
+    context['column_names'] = column_names
+    detector = DetectorBuilder(context=con, dataset=dataset).build()
+    supported_smells = detector.get_supported_data_smell_types()
+    print(supported_smells)
+    detected_smells = detector.detect()
+    sorted_results = sort_results(detected_smells, column_names)
     context['results'] = sorted_results
     return render(request, 'results.html', context)
-
 
 def sort_results(results, columns):
     sorted_results = {}
@@ -104,7 +119,7 @@ def sort_results(results, columns):
     return sorted_results
 
 def precheck_columns(columns):
-    if 'Unnamed: 0' in columns:
-        columns.remove('Unnamed: 0')
-    return columns
+    if "Unnamed: 0" in columns:
+        columns.remove("Unnamed: 0")
+    return sorted(columns)
 
