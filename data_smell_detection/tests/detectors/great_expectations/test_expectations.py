4c8986a21fbc216b00b474cd71cefc6904974330
Martin Kerschbaumer <m.kerschbaumer@student.uibk.ac.at>
2021-04-23 14:29:09 +0200

    Ensure tests of ExpectColumnValuesToNotContainSuspectSignSmell are successful

--- data_smell_detection/tests/detectors/great_expectations/test_expectations.py
+++ data_smell_detection/tests/detectors/great_expectations/test_expectations.py
@@ -1,14 +1,18 @@
 # Check whether the expectations which implement data smell detection work as intended.
 # "Examples" are executed to test the behaviour.
 from typing import List
-
 from great_expectations.expectations.expectation import Expectation
+
+from datasmelldetection.detectors.great_expectations.expectations import (
+    ExpectColumnValuesToNotContainSuspectSignSmell
+)
 
 from .helper_functions import check_expectation_examples
 
 
 class TestExpectations:
     expectations_to_test: List[Expectation] = [
+        ExpectColumnValuesToNotContainSuspectSignSmell()
     ]
 
     def test_examples_of_all_expectations(self):
