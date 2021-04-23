960630755a7ef955b14b5e7cb6344b8df9059080
Martin Kerschbaumer <m.kerschbaumer@student.uibk.ac.at>
2021-04-23 14:24:28 +0200

    Add implementation to ensure that all run tests of checked Expectation classes were successful

---
+++ data_smell_detection/tests/detectors/great_expectations/test_expectations.py
@@ -0,0 +1,16 @@
+# Check whether the expectations which implement data smell detection work as intended.
+# "Examples" are executed to test the behaviour.
+from typing import List
+
+from great_expectations.expectations.expectation import Expectation
+
+from .helper_functions import check_expectation_examples
+
+
+class TestExpectations:
+    expectations_to_test: List[Expectation] = [
+    ]
+
+    def test_examples_of_all_expectations(self):
+        for expectation in self.expectations_to_test:
+            check_expectation_examples(expectation)
