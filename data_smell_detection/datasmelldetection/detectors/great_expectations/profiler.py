7aadde7665f19d72c78531e0aead052b2a6e2da5
Martin Kerschbaumer <m.kerschbaumer@student.uibk.ac.at>
2021-04-28 20:38:44 +0200

    Allow to pass custom data smell configuration (kwargs which should be used for generated expectation configurations)
    Add helper function for creating data smell configuration dictionary
    Adapt DataSmellAwareProfiler to use data smell configuration dictionary
    Allow to pass custom data smell configuration
    Document how to pass data smell configuration dictionary

--- data_smell_detection/datasmelldetection/detectors/great_expectations/profiler.py
+++ data_smell_detection/datasmelldetection/detectors/great_expectations/profiler.py
@@ -1,5 +1,7 @@
-from typing import List, Dict
+from typing import List, Dict, Set, Any
 
+from copy import deepcopy
+from datasmelldetection.core import DataSmellType
 from great_expectations.core import ExpectationConfiguration
 from great_expectations.profile.basic_dataset_profiler import BasicDatasetProfilerBase
 from great_expectations.core.expectation_suite import ExpectationSuite
@@ -8,6 +10,20 @@ from datasmelldetection.detectors.great_expectations.datasmell import (
     DataSmellRegistry,
     default_registry
 )
+
+
+# Create a configuration dictionary for data smells. The registry is used to
+# get the data smell types of all registered data smells. An empty configuration
+# dictionary is created for each registered data smell.
+def _create_data_smell_configuration_dict(registry: DataSmellRegistry) \
+        -> Dict[DataSmellType, Dict[str, Any]]:
+    registered_data_smells: Set[DataSmellType] = registry.get_registered_data_smells()
+
+    result: Dict[DataSmellType, Dict[str, Any]] = dict()
+    for data_smell_type in registered_data_smells:
+        result[data_smell_type] = dict()
+
+    return result
 
 
 class DataSmellAwareProfiler(BasicDatasetProfilerBase):
@@ -29,6 +45,24 @@ class DataSmellAwareProfiler(BasicDatasetProfilerBase):
         to use. If this key does not exist or is None, then the default registry (
         :class:`~datasmelldetection.detectors.great_expectations.datasmell.default_registry`
         ) is used.
+
+    data_smell_configuration:
+        A dictionary of type Dict[DataSmellType, Dict[str, Any]]. It stores
+        kwargs for an expectation class which detects a specific
+        :class:`~datasmelldetection.detectors.core.datasmells.DataSmellType`.
+        If this configuration key is provided, only data smells are considered
+        which are explicitly specified as keys. The corresponding dictionary
+        values (for a specific
+        :class:`~datasmelldetection.detectors.core.datasmells.DataSmellType`)
+        are used as kwargs. If this configuration key is not provided it is
+        assumed that no data-smell specific kwargs should be used. In this case
+        all data smells are used which are registered in the corresponding
+        data smell registry. The data smell configuration dictionary can also
+        be used to restrict data smell detection to specific data smell types.
+        This can be achieved by providing a data smell configuration dictionary
+        which only contains the relevant data smells. Data smells which are
+        not registered in the provided data smell registry may be provided but
+        are ignored.
     """
 
     @classmethod
@@ -48,6 +82,14 @@ class DataSmellAwareProfiler(BasicDatasetProfilerBase):
             # Custom valid data smell registry provided
             registry = configuration["registry"]
 
+        # Kwargs to use for each data smell type.
+        if configuration is None or \
+                "data_smell_configuration" not in configuration or \
+                not isinstance(configuration["data_smell_configuration"], dict):
+            data_smell_configuration: Dict[DataSmellType, Dict[str, Any]] = _create_data_smell_configuration_dict(registry)
+        else:
+            data_smell_configuration = configuration["data_smell_configuration"]
+
         df.set_default_expectation_argument("catch_exceptions", True)
         df.set_config_value("interactive_evaluation", False)
 
@@ -64,9 +106,16 @@ class DataSmellAwareProfiler(BasicDatasetProfilerBase):
             meta_columns[column]["type"] = str(type_)
 
             # Parameters used to evaluate expectation later on.
-            kwargs = {"column": column}
             expectation_dict = registry.get_smell_dict_for_profiler_data_type(type_)
-            for expectation_type in expectation_dict.values():
+            for data_smell_type, expectation_type in expectation_dict.items():
+                if data_smell_type not in data_smell_configuration:
+                    # Data smell type should not be considered
+                    continue
+
+                # Use provided kwargs for the corresponding data smell type
+                kwargs: Dict[str, Any] = deepcopy(data_smell_configuration[data_smell_type])
+                kwargs["column"] = column
+
                 config = ExpectationConfiguration(
                     expectation_type=expectation_type, kwargs=kwargs
                 )
