82b25700ed93bf60c8815d33b52ac0598cddabe7
Laura Geiger <laura.geiger@student.uibk.ac.at>
2021-06-01 11:50:24 +0200

    Started testing

--- web_application/argon-dashboard-django/app/tests.py
+++ web_application/argon-dashboard-django/app/tests.py
@@ -2,7 +2,24 @@
 """
 Copyright (c) 2019 - present AppSeed.us
 """
+from app.models import File, Column, DetectedSmell, SmellType, Parameter
+from app import forms
+from django.contrib.auth.models import User
+from django.test import TestCase, Client
+from django.urls import reverse 
 
-from django.test import TestCase
+class TestViews(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username='testuser', password='12345')
+        login = self.client.login(username='testuser', password='12345')
+        File.objects.create(file_name='TestCSV.csv', user=self.user)
+        File.objects.create(file_name='TestCSV2.csv', user=self.user)
+        self.client = Client()
 
-# Create your tests here.
+    def test_upload_POST(self):
+        response = self.client.get(reverse('upload'))
+        self.assertEquals(response.status_code, 200)
+        self.assertTemplateUsed(response, 'index.html')
+
+    
+
