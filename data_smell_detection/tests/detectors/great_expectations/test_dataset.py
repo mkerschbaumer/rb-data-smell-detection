a970cc7969e829c494c3887949bf912f1282db89
Martin Kerschbaumer <m.kerschbaumer@student.uibk.ac.at>
2021-08-16 13:54:36 +0200

    Remove Titanic.csv Adapt dataset unit tests to not use Titanic.csv

--- data_smell_detection/tests/detectors/great_expectations/test_dataset.py
+++ data_smell_detection/tests/detectors/great_expectations/test_dataset.py
@@ -27,38 +27,43 @@ class TestFileBasedDatasetManager:
     def test_get_available_dataset_identifiers(self):
         identifiers = manager.get_available_dataset_identifiers()
         assert isinstance(identifiers, set)
-        assert len(identifiers) == 2
-        expected_datasets = [
-            "Titanic.csv",
-            "data_smell_testset.csv"
-        ]
+        assert len(identifiers) == 1
+        expected_datasets = ["data_smell_testset.csv"]
         # Ensure all expected datasets are returned by
         # get_available_dataset_identifiers.
         assert all([x in identifiers for x in expected_datasets])
 
     def test_get_dataset(self):
-        dataset = manager.get_dataset("Titanic.csv")
+        dataset = manager.get_dataset("data_smell_testset.csv")
         assert isinstance(dataset, Dataset)
 
 
 class TestDatasetWrapper:
     def test_get_column_names(self):
-        dataset = manager.get_dataset("Titanic.csv")
+        dataset = manager.get_dataset("data_smell_testset.csv")
 
-        expected_column_names = ["Name", "PClass", "Age", "Sex", "Survived", "SexCode"]
+        expected_column_names = [
+            "int1",
+            "int2",
+            "float1",
+            "float2",
+            "string1",
+            "string2",
+            "string3"
+        ]
         column_names = dataset.get_column_names()
         # Ensure all expected column names are present
         assert all([name in column_names for name in expected_column_names])
 
     def test_get_great_expectations_dataset(self):
-        dataset = manager.get_dataset("Titanic.csv")
+        dataset = manager.get_dataset("data_smell_testset.csv")
 
         # Extract Great Expectations dataset
         ge_dataset = dataset.get_great_expectations_dataset()
         assert isinstance(ge_dataset, great_expectations.dataset.Dataset)
 
     def test_get_batch_request(self):
-        dataset = manager.get_dataset("Titanic.csv")
+        dataset = manager.get_dataset("data_smell_testset.csv")
 
         batch_request = dataset.get_batch_request()
         assert isinstance(batch_request, BatchRequest)
@@ -68,4 +73,4 @@ class TestDatasetWrapper:
         assert "batch_identifiers" in batch_request.partition_request
         batch_identifiers = batch_request.partition_request["batch_identifiers"]
         assert "filename" in batch_identifiers
-        assert batch_identifiers["filename"] == "Titanic.csv"
+        assert batch_identifiers["filename"] == "data_smell_testset.csv"
